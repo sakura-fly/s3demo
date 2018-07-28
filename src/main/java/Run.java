1b92e90d6f9da225a9d7f9dcb621b6779d328e54
sakura-fly <sourcesakura@sina.com>
2018-07-28 16:18:44 +0800

    创建桶，桶列表

--- src/main/java/Run.java
+++ src/main/java/Run.java
@@ -1,23 +1,47 @@
+import com.amazonaws.ClientConfiguration;
+import com.amazonaws.Protocol;
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.Bucket;
 import org.junit.Test;
+
+import java.util.List;
 
 public class Run {
     String accessKey = "JQUDUKZ3P2K6N2NAFH68";
     String secretKey = "aaoiD4rdtErqiHNLcN0cjys45CAcvD8DLhItcTLk";
+
     @Test
     public void run() {
-
+        AmazonS3 c = createConnection();
+        // createBucket("gqy", c);
+        List<Bucket> l = listBuckets(c);
+        for (Bucket b : l) {
+            System.out.println(b);
+        }
     }
 
-    public void createConnection() {
+    // @Test
+    public AmazonS3 createConnection() {
 
 
         AWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
+        ClientConfiguration clientConfig = new ClientConfiguration();
+        clientConfig.setProtocol(Protocol.HTTP);
         AmazonS3 conn = new AmazonS3Client(credentials);
-        conn.setEndpoint("objects.dreamhost.com");
+        conn.setEndpoint("http://192.168.1.124:7480");
+        return conn;
+    }
+
+    public List<Bucket> listBuckets(AmazonS3 conn) {
+        return conn.listBuckets();
+
+    }
+
+    public void createBucket(String name, AmazonS3 conn) {
+        conn.createBucket(name);
     }
 
 }
