c8ea28c889fb3ee47f46a6aea0c7a0963c0e2bb4
sakura-fly <sourcesakura@sina.com>
2018-07-30 14:11:40 +0800

    自己看注释

--- src/main/java/Run.java
+++ src/main/java/Run.java
@@ -7,29 +7,35 @@ import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import com.amazonaws.services.s3.model.Bucket;
-import com.amazonaws.services.s3.model.ObjectListing;
-import com.amazonaws.services.s3.model.S3ObjectSummary;
+import com.amazonaws.services.s3.model.*;
 import com.amazonaws.util.StringUtils;
 import org.junit.Test;
 
+import java.io.ByteArrayInputStream;
+import java.io.File;
 import java.util.List;
 
 public class Run {
     String accessKey = "JQUDUKZ3P2K6N2NAFH68";
     String secretKey = "aaoiD4rdtErqiHNLcN0cjys45CAcvD8DLhItcTLk";
-    String BUCKET_NAME = "my-first-s3-bucket1";
-    // String BUCKET_NAME = "gqy";
+    // String BUCKET_NAME = "my-first-s3-bucket1";
+    String BUCKET_NAME = "gqy";
+    String fileName = "hello.txt";
 
     @Test
     public void run() {
         AmazonS3 c = createConnection();
+        getUrl(c,BUCKET_NAME,fileName);
+
+        // downObject(c, BUCKET_NAME);
+
+        // createObject(c, BUCKET_NAME);
         // listBucketContent(c, BUCKET_NAME);
         // createBucket("gqy", c);
-        List<Bucket> l = listBuckets(c);
-        for (Bucket b : l) {
-            System.out.println(b);
-        }
+        // List<Bucket> l = listBuckets(c);
+        // for (Bucket b : l) {
+        //     System.out.println(b);
+        // }
     }
 
     // 创建连接
@@ -67,12 +73,29 @@ public class Run {
         do {
             for (S3ObjectSummary objectSummary : objects.getObjectSummaries()) {
                 // System.out.println(objectSummary);
-                System.out.println(objectSummary.getKey() + "\t" +
-                        objectSummary.getSize() + "\t" +
-                        StringUtils.fromDate(objectSummary.getLastModified()));
+                System.out.println(objectSummary);
             }
             objects = conn.listNextBatchOfObjects(objects);
         } while (objects.isTruncated());
     }
 
+    // 创建对象
+    public void createObject(AmazonS3 conn, String bucketName) {
+        ByteArrayInputStream input = new ByteArrayInputStream("Hello World!".getBytes());
+        conn.putObject(bucketName, "hello.txt", input, new ObjectMetadata());
+    }
+
+    // 下载对象
+    public void downObject(AmazonS3 conn, String bucketName) {
+        conn.getObject(
+                new GetObjectRequest(bucketName, "hello.txt"),
+                new File("G:\\test\\s3\\hello.txt")
+        );
+    }
+    // 获取下载链接
+    public String getUrl(AmazonS3 conn, String bucketName,String fileName){
+        GeneratePresignedUrlRequest request = new GeneratePresignedUrlRequest(bucketName, fileName);
+        System.out.println(conn.generatePresignedUrl(request));
+        return conn.generatePresignedUrl(request).toString();
+    }
 }
